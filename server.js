15d13f57cf0350f4853a5e9f3047ddf7def76817
abdullah2004-kalvium <abdullahm6270@gmail.com>
2025-02-06 18:54:24 +0530

    mongoose.complete

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require("express")
 const app = express();
 const PORT = 4000
+const connectDB = require("./database");
+connectDB();
+
 app.get("/ping",(req,res)=>{
     res.send("Pong!")
 })
+
 
 app.listen(PORT,()=>{
     console.log(`Server is running on http://localhost:${PORT}`)

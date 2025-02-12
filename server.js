580d42abe7bb6da415e250bda94962d2c52bed94
abdullah2004-kalvium <abdullahm6270@gmail.com>
2025-02-12 10:53:27 +0530

    It's done

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
-const express = require("express")
-const app = express();
-const PORT = 4000
-app.get("/ping",(req,res)=>{
-    res.send("Pong!")
+const express=require('express');
+const {connectDB,getConnection}=require('./data_base');
+const app= express();
+const port =3000;
+app.get('/ping',(req,res)=>{
+    res.send('Pong!');
 })
-
-app.listen(PORT,()=>{
-    console.log(`Server is running on http://localhost:${PORT}`)
+connectDB();
+app.get('/',(req,res)=>{
+    
+    res.send(getConnection());
 })
+app.listen(port,()=>{
+    console.log(`server is running at http://localhost:${port}`);
+});
